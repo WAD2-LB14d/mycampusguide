7f9a13fe1dc5e0e320ce96ffcae0c0ab7a4ed5f8
Joachim Vanneste <2460800V@student.gla.ac.uk>
2021-03-11 16:32:43 +0000

    models added

--- my_campus_guide/guide/models.py
+++ my_campus_guide/guide/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Course(models.Model):
+	NAME_MAX_LENGTH = 128
+	name = models.CharField(max_length = NAME_MAX_LENGTH, unique = True)
+	views = models.IntegerField(default = 0)
+	slug = models.SlugField(unique = True)
+
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.name)
+		super(Course, self).save(*args, **kwargs)
+
+	class Meta:
+		verbose_name_plural = "Courses"
+
+	def __str__(self):
+		return self.name
+
+
+
+class Lecturer(models.Model):
+	NAME_MAX_LENGTH = 128
+	name = models.CharField(max_length = NAME_MAX_LENGTH, unique = True)
+	views = models.IntegerField(default = 0)
+	slug = models.SlugField(unique = True)
+
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.name)
+		super(Lecturer, self).save(*args, **kwargs)
+
+	class Meta:
+		verbose_name_plural = "Lecturers"
+
+	def __str__(self):
+		return self.name
