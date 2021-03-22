fb9e18fc910028673a107ded729751eb44d4290a
kubajj <vilouskubajj@gmail.com>
2021-03-22 16:33:18 +0100

    Make admin usable

--- my_campus_guide/guide/admin.py
+++ my_campus_guide/guide/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from guide.models import Lecturer, Course, Category 
 # Register your models here.
+
+admin.site.register(Category)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'currentlecturer', 'school', 'views')
+admin.site.register(Course, CourseAdmin)
+class LecturerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'teaching', 'views')
+admin.site.register(Lecturer)
