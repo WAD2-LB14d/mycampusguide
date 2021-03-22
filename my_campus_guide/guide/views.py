ed3b8e2722115043ba63b04c11f791bb69209b4b
kubajj <vilouskubajj@gmail.com>
2021-03-22 16:58:38 +0100

    Make lecturers page show lecturers table

--- my_campus_guide/guide/views.py
+++ my_campus_guide/guide/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 from guide.forms import LecturerForm
-
+from guide.models import Lecturer
 # Create your views here.
 
 def index(request):
   return render(request, 'guide/index.html')
 
 def courses(request):
-	return render(request, 'guide/courses.html')
+  return render(request, 'guide/courses.html')
 
 def lecturers(request):
-	return render(request, 'guide/lecturers.html')
+  lecturers = Lecturer.objects.order_by('name')
+  context_dict = {}
+  context_dict['lecturers'] = lecturers
+  return render(request, 'guide/lecturers.html', context=context_dict)
